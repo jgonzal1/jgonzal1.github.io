5981c1a384b733b2001f0fc81478bb4ff2a7e21f
Javier Gonzalez Berenguel <jgb@aety.io>
2022-06-27 12:30:54 +0200

    + favicon, ~hrefHandler linting

--- hrefHandler.js
+++ hrefHandler.js
@@ -1,12 +1,13 @@
 const url = window.location.href;
 const href = url.substring(url.indexOf("#") + 1);
-const lTmp = href.substring(href.search("lang=")===-1?0:href.search("lang=")+5);
-const lang = lTmp===href?href:lTmp.substring(0,2);
-const rTmp = href.substring(href.search("role=")===-1?0:href.search("role=")+5);
-const role = rTmp===href?href:rTmp.substring(0,rTmp.search("&")===-1?rTmp.length:rTmp.search("&"));
+const lTmp = href.substring(href.search("lang=") === -1 ? 0 : href.search("lang=") + 5);
+const lang = lTmp === href ? href : lTmp.substring(0, 2);
+const rTmp = href.substring(href.search("role=") === -1 ? 0 : href.search("role=") + 5);
+const role = rTmp === href ? href : rTmp.substring(0, rTmp.search("&") === -1 ? rTmp.length : rTmp.search("&"));
 
 let cvHeader, cvBody, legend;
-if(lang === 'ES') {
+if (lang === 'ES') {
+    // @ts-ignore
     [cvHeader, cvBody, legend] = getSpanishContents();
 } else {
     // @ts-ignore

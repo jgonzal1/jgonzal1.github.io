11f1a052415a2cd3e00ade67b035e2d0f67ad3c2
javier_gonzalez-at-437178867634 <javier.gonzalezberenguel@gmail.com>
2023-03-25 19:11:43 +0100

    + first referenced project

--- locale/EN.js
+++ locale/EN.js
@@ -268,8 +268,12 @@ function getEnglishContents() {
   </tr>
   <tr>
     <td class="bold-right"> </td>
-    <td class="w70">Jun 19 – Oct 19<br>Development and <span class="arch">architecture</span> for an ETL and BI project<br>Involving <span class="bi">extraction of data files from different <span class="cloud">cloud sources</span> (SAP, Salesforce, Excel documents)</span> to Amazon S3 buckets, transformation of data with <span class="bi">Amazon Glue (boto3 and
-      pyspark libraries)</span>, and load in <span class="bi">Amazon Redshift tables which are taken by <span class='webdev'>Power BI for its data representation</span></span>.
+    <td class="w70">Jun 19 – Oct 19<br>Development, <a href="https://github.com/jgonzal1/d3js-graph-analytic">analysis</a> and <span class="arch">architecture</span> for an ETL and BI project<br>
+    Involving <span class="bi">extraction of data files from different <span class="cloud">cloud sources</span>
+    (SAP, Salesforce, Excel documents)</span> to Amazon S3 buckets, transformation of data with
+    <span class="bi">Amazon Glue (boto3 and pyspark libraries)</span>, and load in
+    <span class="bi">Amazon Redshift tables which are taken by
+    <span class='webdev'>Power BI for its data representation</span></span>.
     </td>
   </tr>
   <tr>

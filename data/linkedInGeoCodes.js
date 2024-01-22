ca0b6ec02396c2f3e61e04aa3caf806adbd1e8e9
Javier <javier.gonzalezberenguel@gmail.com>
2024-01-22 13:57:20 +0100

    + linkedin geocodes

---
+++ data/linkedInGeoCodes.js
@@ -0,0 +1,22 @@
+const linedInGeoCodes = [
+  { "f_PP_": 104236363, "place": "Lausanne............." },
+  { "f_PP_": 104691271, "place": "Bern (general?)......" },
+  { "f_PP_": 104750956, "place": "Montreux............." },
+  { "f_PP_": 104989726, "place": "Fribourg............." },
+  { "f_PP_": 106633403, "place": "Geneva..............." },
+  { "f_PP_": 108025228, "place": "Lausanne (all)......." },
+  { "f_PP_": 111746588, "place": "Geneve..............." },
+  { "geoId": 101554085, "place": "Vaud (prov. Lausanne)" },
+  { "geoId": 102056024, "place": "Neuchâtel............" },
+  { "geoId": 103351531, "place": "Fribourg............." },
+  { "geoId": 104229378, "place": "Lausanne............." },
+  { "geoId": 90009886, "_place": "Geneva (metrop.)....." },
+  { "geoId": 90009887, "_place": "Bern (metrop.)......." },
+  { "geoId": 90009889, "_place": "Lausanne (metrop.)..." },
+];
+
+/*
+https://www.linkedin.com/jobs/search/?f_PP=104236363%2C104691271%2C104750956%2C104989726%2C106633403%2C108025228%2C111746588
+&f_E=4 (experience mid-senior)
+&f_WT=3%2 (work type hybrid and remote)
+*/

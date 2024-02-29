1fbb84a3198193e2a0b4608a0557f292f5f4b6fb
Javier González Berenguel <jgb@aety.io>
2024-02-29 09:51:11 +0100

    + tasks by category and date instead of map

--- assets/tasksManager.js
+++ assets/tasksManager.js
@@ -103,6 +103,7 @@ class tasksManager extends React.Component {
       lastRefreshDateTime: "undefined",
       lastUpdatedItem: false,
       mondayTasksCols: {},
+      mondayTasksByCategoryAndDay: [],
       mondayTasksByCategory: [],
       mondayTasksDurationSum: 0,
       mondayTasksByDay: {},
@@ -267,6 +268,148 @@ class tasksManager extends React.Component {
     return Object.assign(svg.node(), { scales: { color } });
     //#endregion
   };
+  aggrTasksByCategoryAndDay = (sortedMondayItemsJson) => {
+    const startYear = 2024;
+    const msPerDay = (24 * 3600 * 1000);
+    const daysSince1970UntilStartYear = (startYear - 1970) * 365.25;
+    const tasksDurationByDayCategoryPk = sortedMondayItemsJson.map(t => {
+      return {
+        "x": //Math.floor(
+          //new Date(
+          t["datetime"].substring(0, 10),
+        //).getTime() / msPerDay // Days since 1970
+        //) - daysSince1970UntilStartYear // Days since startYear
+        "name": t["cat"],
+        "value": Math.round(t["dur"] * 60)
+      };
+    }).reduce(
+      (accumulator, item) => {
+        const pk = `${item["x"]}|${item["name"]}`;
+        if (!accumulator[pk]) {
+          accumulator[pk] = 0;
+        }
+        accumulator[pk] += item["value"]
+        return accumulator
+      }, {}
+    );
+    const tasksDurationByDayCategory = Object.keys(tasksDurationByDayCategoryPk).map(tDCD => {
+      const [x, name] = tDCD.split("|");
+      return {
+        "x": x,
+        "name": name,
+        "value": tasksDurationByDayCategoryPk[tDCD] ?? 0
+      };
+    });
+    const days = tasksDurationByDayCategory.map(t => t["x"]).filter((val, idx, arr) => arr.indexOf(val) === idx)
+    const categories = [
+      "1.🏠", "2.💰", "3.🍏", "4.🚩🇩🇰", "5.🔬", "6.📺", "7.🎮", "8.🌐", "9.➕"
+    ];
+    days.map(d =>
+      categories.map(c =>
+        tasksDurationByDayCategory.push({ "x": d, "name": c, "value": 0 })
+      )
+    );
+    /*console.log(tasksDurationByDayCategory);*/
+
+    const colors = new Map([
+      ["1.🏠", "#59a14f"],
+      ["2.💰", "#9c755f"],
+      ["3.🍏", "#edc949"],
+      ["4.🚩🇩🇰", "#f28e2c"],
+      ["5.🔬", "#ff9da7"],
+      ["6.📺", "#af7aa1"],
+      ["7.🎮", "#4e79a7"],
+      ["8.🌐", "#76b7b2"],
+      ["9.➕", "#bab0ab"]
+    ]);/*console.log(colors);*/
+
+    const series = d3.stack()
+      .keys(colors.keys())
+      .value((group, key) => group.get(key)?.value ?? 0)
+      .order(d3.stackOrderReverse)
+      (
+        d3.rollup(tasksDurationByDayCategory,
+          ([d]) => d, d => d.x, d => d.name).values()
+      ).map(s => (
+        s.forEach(d =>
+          d.data = d.data.get(s.key)
+        ), s
+      )).map(k => k.filter(l => !!l.data))
+    console.log(series);
+
+    const width = 600;
+    const height = 1000;
+    const marginTop = 100;
+    const marginRight = 100;
+    const marginBottom = 100;
+    const marginLeft = 100;
+
+    const x = d3.scaleBand()
+      .domain(tasksDurationByDayCategory.map(d => d.x))
+      .rangeRound([marginLeft, width - marginRight]);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(series, d => d3.max(d, d => d[1]))]).nice()
+      .range([height - marginBottom, marginTop]);
+
+    const color = d3.scaleOrdinal()
+      .domain(colors.keys())
+      .range(colors.values());
+
+    // Create the SVG container.
+    const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", "max-width: 100%; height: auto;");
+
+    // Create the bars.
+    // const formatRevenue = x => (+(x / 1e9).toFixed(2) >= 1)
+    //   ? `${(x / 1e9).toFixed(2)}B`
+    //   : `${(x / 1e6).toFixed(0)}M`;
+
+    svg.append("g")
+      .selectAll("g")
+      .data(series)
+      .join("g")
+      .attr("fill", ({ key }) => color(key))
+      .call(g => g.selectAll("rect")
+        .data(d => d)
+        .join("rect")
+        .attr("x", d => x(d.data.x))
+        .attr("y", d => y(d[1]))
+        .attr("width", x.bandwidth() - 1)
+        .attr("height", d => y(d[0]) - y(d[1]))
+        .append("title")
+        .text(d => `${d.data.name}, ${d.data.x}, ${/*formatRevenue(*/
+          d.data.value/*)*/
+          }`));
+
+    // Create X axis
+    svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)");
+    //.tickValues(d3.ticks(...d3.extent(x.domain()), width / 80))
+    //.tickSizeOuter(10));
+
+    svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(d3.axisLeft(y))
+      // .tickFormat(x => (x / 1e9).toFixed(0)))
+      .call(g => g.select(".domain").remove())
+      .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text("minutes"));
+
+    return Object.assign(svg.node(), { scales: { color } });
+  }
   aggrTasksByDay = (sortedMondayItemsJson) => {
     const [
       nextClimbingDay, nextVI, nextVF
@@ -377,10 +520,12 @@ class tasksManager extends React.Component {
     const sortedMondayItemsJson = this.addMondayMeta(mondayTasksCols);
     const mondayTasksByDay = this.aggrTasksByDay(sortedMondayItemsJson);
     const mondayTasksByCategory = this.aggrTasksByCategory(sortedMondayItemsJson);
+    const mondayTasksByCategoryAndDay = this.aggrTasksByCategoryAndDay(sortedMondayItemsJson);
     // @ts-ignore
     this.setState({
       mondayTasksCols: sortedMondayItemsJson,
       mondayTasksByCategory: [mondayTasksByCategory],
+      mondayTasksByCategoryAndDay: [mondayTasksByCategoryAndDay],
       mondayTasksByDay: mondayTasksByDay,
       getDatedMondayItemsToJson: false,
       lastRefreshDateTime: new Date().toISOString().substring(2, 19).replace("T", " ")
@@ -466,11 +611,17 @@ class tasksManager extends React.Component {
       //@ts-ignore
       this.getDatedMondayTasksToMultipleJson(monday_key, boardId, columnRenames);
     }
-    if (this.state.mondayTasksByCategory.length) {
+    if (this.state.mondayTasksByCategory.length) { // Add Bubble Chart
       const tasksByCategoryPlaceholder = document.querySelector("#tasksByCategory");
       if (!tasksByCategoryPlaceholder) { return; }
       tasksByCategoryPlaceholder.innerHTML = "";
       tasksByCategoryPlaceholder.appendChild(this.state.mondayTasksByCategory[0]);
+    }
+    if (this.state.mondayTasksByCategoryAndDay.length) { // Add Stacked Bar Chart
+      const tasksByCategoryAndDayPlaceholder = document.querySelector("#tasksByCategoryAndDay");
+      if (!tasksByCategoryAndDayPlaceholder) { return; }
+      tasksByCategoryAndDayPlaceholder.innerHTML = "";
+      tasksByCategoryAndDayPlaceholder.appendChild(this.state.mondayTasksByCategoryAndDay[0]);
     }
     //#endregion
     // @ts-ignore

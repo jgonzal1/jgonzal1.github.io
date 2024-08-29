2362f2eba0342d771f8f8781b56ce1797edc6fd1
DARCS Almería <dark.javier.haf@gmail.com>
2024-08-29 11:22:07 +0200

    @

--- locale/EN.js
+++ locale/EN.js
@@ -17,11 +17,11 @@ function getEnglishContents() {
       </fieldset>
       <fieldset style="font-family:monospace">
         <legend>🗣</legend>
-        <span style="color:#FF0">★★★★★</span> EN<br>
-        <span style="color:#FF0">★★★★★</span> ES<br>
-        <span style="color:#FF0">★★</span>☆☆☆ DE<br>
-        <span style="color:#FF0">★★</span>☆☆☆ FR<br>
-        <span style="color:#FF0">★★</span>☆☆☆ DA<br>
+        <span style="color:#F0C674">★★★★★</span> EN<br>
+        <span style="color:#F0C674">★★★★★</span> ES<br>
+        <span style="color:#F0C674">★★</span>☆☆☆ DE<br>
+        <span style="color:#F0C674">★★</span>☆☆☆ FR<br>
+        <span style="color:#F0C674">★★</span>☆☆☆ DA<br>
       </fieldset>
     </td>
     <td>
@@ -85,20 +85,15 @@ function getEnglishContents() {
     <td class="w70">
       (21-11 – Nowadays / Part-time)<br>
       Full-stack developer, data engineer, and AWS/monday.com/JSM/Power BI technical consultant.<br><br>
-
       24-03 - Nowadays<br>
       Project: ETL SYS102 for VURDST<br>
       Role: Data engineer at UFST<br>
       Responsible for data pipeline development on AWS to gather all the data sources required for<br>
-      a Business Intelligence solution for the properties tax agency in Denmark.<br>
-      <br>
-
+      a Business Intelligence solution for the properties tax agency in Denmark.<br><br>
       24-01 - 24-02<br>
       Project: Jira Ultimate Customizer<br>
       Role: Full-stack Developer<br>
-      Responsible for Jira's Ultimate Customizer and Issue Lock help desk resolutions.<br>
-      <br>
-
+      Responsible for Jira's Ultimate Customizer and Issue Lock help desk resolutions.<br><br>
       23-09 - 23-12<br>
       Project: TDC´s Smart Build solution, Role: Full-stack developer and UX implementation responsible.<br>
       Full-stack developer for TDC NET geographical auctioning Phoenix solution at the AI & Digital team.<br>
@@ -112,7 +107,6 @@ function getEnglishContents() {
       <li>Javascript/Typescript: React, Chakra, OpenLayers, LeafLet, atomic design</li>
       <li>Database: SQL queries, Snowflake</li>
       </ol><br>
-
       22-05 - 22-08<br>
       Project: Jira Ultimate Customizer<br>
       Role: Full-stack Developer<br>
@@ -126,7 +120,6 @@ function getEnglishContents() {
       <li>Automation of testing in long-term stable JSM versions 4.5, 4.13, 5.4, 5.8</li>
       <li>automated usage of nvmrc</li>
       </ol><br>
-
       22-09 - 23-04<br>
       Project: Data automation on Dr. Oetker Home, public webpages content creation.<br>
       Role: <span class="arch">Full-stack</span> software developer, data engineer, and AWS technical consultant.<br>
@@ -140,12 +133,10 @@ function getEnglishContents() {
       <li>Full live elasticsearch <span class="arch">database migration</span> from a provider to another one.</li>
       <li>Big <span class="arch">reduction of the technical debt</span> of the project's back-end.</li>
       <li>Creation of a web-service for Dr. Oetker internal stakeholders for enabling them to modify nginx configuration for different Dr. Oetker websites.</li></ol><br>
-
       22-03 - 22-09<br>
       Project:  Jira Ultimate Customizer,<br>
       Role: Full-stack Developer<br>
       Responsible for enabling Jira's Ultimate Customizer with its correct UI/UX in signup, approvals, single portals, portals page, users login, profile page, create request, view request, requests page, and for fixing permissions error when editing login screen for the minor version 4.3.0 as well as for <span class="arch">resolving and communicating</span> on the resolution for <span class="arch">all the Service Desk incidences</span> for this App.<br><br>
-
       22-01 - 22-03<br>
       Project: Integration between monday.com and KortInfo,<br>
       Role: Software Developer<br>
@@ -158,12 +149,10 @@ function getEnglishContents() {
       <li>Monitoring these changes through some RSS connected to a business intelligence report to check if there could be potential issues after going live.</li>
       <li>Technologies used:</li>
       <li>JavaScript, <span class="arch">API REST</span>, GraphQL, Zapier.</li></ol><br>
-
       21-12 - 22-01<br>
       Project: Business intelligence solution,<br>
       Role: Software Developer, Data engineer / Consultant.<br>
       Responsible for developing a Business Intelligence solution to provide actionable market intelligence for Atlassian's Marketplace Apps.<br><br>
-
       Project description:<br>
       There is a regular high interest in automatically gathering all public add-ons information from <span class="arch">Atlassian Marketplace API</span> to understand our partnership vendors better. Javier used the programming language Python in a <span class="arch">serverless architecture</span> for the ETL and an analytical solution for performing SQL queries over the data to identify potential vendors and add-ons.<br>
       Main tasks:<ol>
@@ -184,61 +173,47 @@ function getEnglishContents() {
     <td class="w70">
       (19-11 - 21-11)<br>
       Full-stack developer, consultant, and Power BI technician.<br><br>
-
       21-05 - 21-10<br>
       <span class="arch">Full-stack developer</span> for Ferrovial's application to handle view, monitor and send real-time traffic alerts, automation of the creation of new road trackings, and user management, all accesible within a React.JS webpage. <span class="cloud">Docker, Python</span>, JavaScript, SQL, <span class="arch cloud">APIs REST</span>, <span class="arch cloud">GCP's Directions API</span>, Django, React and UX specialist.<br><br>
-
       21-05 - 21-05<br>
       <span class="bi">Power BI technical consultant</span><br>
       <span class='arch bi'>Power BI teacher</span> for T-Systems, and all other Deutsche Telekom companies.<br><br>
-
       20-12 - 21-04<br>
       <span class="bi">Power BI technical consultant</span><br><br>
       Technical consultant for <span class='arch webdev'>full-stack development</span> on <span class='bi webdev cloud'>Pentaho CDE</span> reports for a Smart City.<br>
       Technical consultant whose role was to <span class="arch">implement proofs of concept</span> for full-stack development of personalized reports on <span class="bi">Pentaho CDE</span> <span class='webdev'>ctools</span> with <span class="bi">postgresql</span> data sources, such as CC charts, <span class='webdev'>Raphaël.JS</span> Sankey and network diagrams, Scatterplot and Tile Set Map Components,
       and environment test cases.<br><br>
-
       Project description:<br>
       A big town hall publicly published its will to implement a "smart city" technology over some years, involving IoT devices around the city.<br>
       The nourishing of the database needed to be filtered and queried to measure specific KPIs for the local administration to decide.<br>
       The goal was to query, filter, and display the data to provide insights on the KPIs with Pentaho CDE (open-source web BI platform).<br><br>
-
       Tasks:<br>
       Technical validations and proof of concepts for shaping how to achieve the requirements for the client (part-time through 2 years).<br>
       Being part of the elected development team to make the solution for the city for the 3 years of project duration.<br>
       Full-stack development of personalized reports on Pentaho CDE.<br>
       Test case development<br><br>
-
       Technologies used:<br>
       Pentaho CDE, PostgreSQL, CC charts, JavaScript, SQL, Leaflet.JS maps, Raphaël.JS, Sankey and network diagrams.<br><br>
-
       20-10 - 20-12<br>
       <span class="bi">Power BI</span> <span class="arch">consultant and technician</span><br><span class='webdev'>Setting up corporate reports</span> for T-Systems to track worker's project dedications, plans, assignments, etc. Using <span class="bi cloud">Azure Data Factory</span> and DataFlows.<br><br>
-
       20-09 - 20-10<br>
       Software developer and consultant.<br>
       <span class="arch">Software developer and consultant</span> for a multi-language automatic translator within a public sector autonomy.<br><br>
-
       20-06 - 20-09<br>
       <span class="bi">Power BI consultant and technician</span><br><span class='webdev'>Setting up multi-client reports</span> for T-Systems based on <span class="cloud">Azure Log Analytics</span> Exports. Design of the Power Query data sources and of all the tab reports.<br><br>
-
       20-05 - 20-06<br>
       <span class="arch">Software Developer and Consultant</span><br>
       Software developer and consultant for a multi-language automatic translator within a public sector autonomy.<br><br>
-
       20-03 - 20-04<br>
       <span class="bi">Big Data technician</span><br>Big Data technician for setting up the data scientist environment for the biggest insurance company in Spain, using <span class="arch bi cloud">Python, AWS Lambda, AWS Step Functions, AWS S3, AWS Athena</span>, AWS IAM and remote connections.<br><br>
-
       19-12 - 20-02<br>
       Technical consultant of <span class="arch cloud">cloud architecture</span>, full-stack developer and big data engineer for a Smart City.<br>
       Technical consultant whose role was to implement proofs of concept of <span class='webdev'>front-end developments</span>, <span class="cloud">python</span> backend
       connectors, third party's <span class='arch cloud webdev'>API managements</span>, big data solutions (<span class='webdev cloud'>elasticsearch</span>, BERT, <span class='webdev'>Kibana</span>), <span class="bi">AWS Lambda functions</span>, connections and <span class="cloud">setting up Docker containers for all
       the components in Docker containers</span> for a big Smart City project.<br><br>
-
       19-12 - 20-01<br>
       <span class='webdev'>Chatbot full-stack developer</span><br>Chatbot development from scratch of a fully <span class='webdev'>serverless web chat</span> using <span class='webdev'>AJAX</span> calls for Microsoft L.U.I.S.,
       <span class='webdev'> CSS3, HTML5 and JavaScript native development</span>.<br><br>
-
       19-11 - 19-12<br>
       Azure and Power BI technical consultant.<br>
       <span class="bi">Power BI, ETL and <span class='arch webdev'>report designer</span></span> for an <span class="cloud">Azure Log Analytics</span> tracking environment in real-state.
@@ -263,7 +238,6 @@ function getEnglishContents() {
       Technical and consultant on Big Data and Analytics solutions. Artificial intelligence <span class='webdev'>software development, chat-bots</span> and <span class="bi">ETLs</span>, using <span class="cloud">Amazon Web Services</span> technologies (AWS EC2, Amazon CodeCommit,
       <span class="bi">Amazon Redshift, Amazon Glue and Amazon S3</span> among others), Microsoft Azure (L.U.I.S., QnA, <span class='webdev'>App Service</span>, <span class="bi">Data Lake Storage, Azure Data Factory, Data Lake Analytics</span>), IBM Watson, <span class='webdev cloud'>Oracle Intelligent Bots</span>,
       <span class="cloud">Google Cloud</span> and SAS. <span class='webdev'>Programming with JavaScript, Node.JS</span>, <span class="bi"><span class="cloud">Python</span>, U-SQL</span> and R.<br><br>
-
       19-06 – 19-10<br>
       Development, <a href="https://github.com/jgonzal1/d3js-graph-analytics">analysis</a> and <span class="arch">architecture</span> for an ETL and BI project.<br>
       Involving <span class="bi">extraction of data files from different <span class="cloud">cloud sources</span>
@@ -271,32 +245,25 @@ function getEnglishContents() {
       <span class="bi">Amazon Glue (boto3 and pyspark libraries)</span>, and load in
       <span class="bi">Amazon Redshift tables which are taken by
       <span class='webdev'>Power BI for its data representation</span></span>.<br><br>
-
       18-11 - 19-05<br>
       Development and <span class="arch">architecture</span> in an <span class="bi">ETL project.<br>
       Involving data extraction from both an Oracle database and Google Sheets</span> spreadsheets, transformation of data with <span class="bi">Data Lake Analytics and Azure App Service within Azure
       Data Factory</span>, and load of data in <span class="bi">Data Lake Storage CSVs which are taken by Power BI for its <span class='webdev'>data representation</span></span>.<br><br>
-
       18-03 - 18-10<br>
       Full-stack Developer for a chatbot for an insurance company.<br><br>
-
       Project description:<br>
       A big insurance company in Spain wanted to implement a chatbot on their webpage to get a lower workload on the phone for easily answered questions. The task was to create it and implement it on the bottom of their webpage (loads when the page fully renders). The development was done using Javascript, Node.js, Python, SQL, and R.<br><br>
-
       Tasks:<br>
       Develop a fully functional and customizable chatbot (now in vivaz.com, loads when page fully renders) from a basic Microsoft template.<br>
       Teach an intern how to develop and coordinate tasks with him.<br><br>
-
       Technologies used:<br>
       Amazon Web Services (AWS EC2, Amazon CodeCommit, Amazon Redshift, Amazon Glue, and Amazon S3 among others).<br>
       Microsoft Azure (L.U.I.S., QnA, App Service, Data Lake Storage, Data Lake Analytics).<br>
       IBM Watson, Oracle Intelligent Bots, Google Cloud, and SAS.<br>
       Programming in Javascript, Node.js, Python, U-SQL, and R.<br><br>
-
       17-12 – 18-02<br>
       Full-stack developer.<br>
-      <span class='webdev'>Development of a geo-analytical BI web service built with R.<br><br>
-
+      <span class="webdev">Development of a geo-analytical BI web service built with R</span>.<br><br>
       17-08 - 17-11<br>
       <span class="arch bi">Data mining technician</span> and machine learning.<br>
       Development of an artificial vision software in <span class="cloud">Python</span> to classify vehicles.<br><br>
@@ -304,7 +271,6 @@ function getEnglishContents() {
       Create a Python artificial intelligence able to process laser vehicle profiles.<br>
       Train the model with thousands of vehicles.<br>
       Enhance profiling detection of the vehicles.<br><br>
-
       Technologies used:<br>
       RHEL bash scripting.<br>
       WinSCP.<br>
@@ -390,11 +356,8 @@ function getEnglishContents() {
   <tr>
     <td class="bold-right">LinkedIn verified evaluation</td>
     <td colspan="2">20-06<br><span class='webdev'>Javascript</span><br><br>
-
     20-06<br>Git<br><br>
-
     20-06<br>Excel<br><br>
-
     20-06<br><span class="cloud">Python</span>
     </td>
   </tr>
@@ -428,7 +391,6 @@ function getEnglishContents() {
     <td colspan="2">
       15-11<br>
       <span class='webdev'>Knowledge management for companies</span><br><br>
-
       15-09<br>Financial accountability
     </td>
   </tr>
@@ -447,6 +409,14 @@ function getEnglishContents() {
   <tr>
     <td class="bold-right">High Level</td>
     <td colspan="2">English (B2 Cambridge certificate)</td>
+  </tr>
+  <tr>
+    <td class="bold-right">Basic Level</td>
+    <td colspan="2">German</td>
+  </tr>
+    <tr>
+    <td class="bold-right">Basic Level</td>
+    <td colspan="2">French</td>
   </tr>
   <tr>
     <td class="bold-right">Basic Level</td>

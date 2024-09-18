eba4d781d981816d6fe0ec93dff397b4c47a2c1a
Javier González Berenguel <jgb@aety.io>
2024-09-18 12:11:33 +0200

    + sideBar with babel

--- assets/sideBarHandler.jsx
+++ assets/sideBarHandler.jsx
@@ -1,5 +1,14 @@
-function sideBarHandler() {
-  const sideBarDom = document.getElementsByClassName("sidebar")[0];
-  const urlPrefix = location.href.substring(0, location.href.search("/pages/"));
-  console.log(urlPrefix)
-}
+const urlPrefix = location.href.substring(0, location.href.search("/pages/"));
+console.log(urlPrefix)
+const sideBarDom = document.getElementsByClassName('sidebar')[0];
+const sideBarRoot = globalThis.ReactDOM.createRoot(sideBarDom);
+sideBarRoot.render(
+  <div className="space-children-1em">
+    <a href="#" onClick={() => document.location = urlPrefix + '/index.html'}>cv</a>
+    <a href="#" onClick={() => document.location = urlPrefix + '/pages/books.html'}>books</a>
+    <a href="#" onClick={() => document.location = urlPrefix + '/pages/music.html'}>music</a>
+    <a href="#" onClick={() => document.location = urlPrefix + '/pages/navezgane.html'}>navezgane</a>
+    <a href="#" onClick={() => document.location = urlPrefix + '/pages/tasksDashboard.html'}>tasksDashboard</a>
+    <a href="#" onClick={() => document.location = urlPrefix + '/pages/theThreeSeas.html'}>theThreeSeas</a>
+  </div >
+)

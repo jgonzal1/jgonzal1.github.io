4e834bed60e5fae3e54d0620da6057f56e95d8c7
DARCS Almería <dark.javier.haf@gmail.com>
2024-09-29 08:37:45 +0200

    @ babel test creation from str

--- assets/babel.jsx
+++ assets/babel.jsx
@@ -1,6 +1,36 @@
-function renderHello($, React, ReactDOM) {
+let babelTemplate = `
+<div>
+    <h1>Heading</h1>
+    <hr />
+    <ComponentOne msg="MsgText-ComponentOne" />
+</div>
+`;
+let babelTemplateStr = `
+<div>
+    <h1>Heading</h1>
+    <hr />
+    ${componentOneStr}
+</div>
+`;
+
+const App = () => {
+  const appCode = Babel.transform(babelTemplateStr, { presets: ['es2017', 'react'] }).code;
+  console.log(appCode);
+  return (eval(Babel.transform(babelTemplate, { presets: ['es2017', 'react'] }).code));
+};
+
+
+ReactDOM.render(
+  React.createElement(App, null),
+  document.getElementById("root")
+);
+
+
+/*function renderHello($, React, ReactDOM) {
   return ReactDOM.render(
-    <div>Hello, React!</div>,
-    $('#container')[0]
+    <div>Hello React!</div>
+    //<span>Hello</span>, <span>React</span>
+    ,
+    $('#root')[0]
   )
-}
+}*/

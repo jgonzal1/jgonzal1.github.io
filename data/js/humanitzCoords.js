9268affdc5ddf5d0bbfee12f90cc26dc9f771b6f
Javier <javier.gonzalezberenguel@gmail.com>
2025-06-06 20:03:17 +0200

    # updated map

--- data/js/humanitzCoords.js
+++ data/js/humanitzCoords.js
@@ -1,4 +1,5 @@
 const waypoints = [
-    { "coords": [-1745, 1100], "name": "Coche", "icon": "car", "size": 2 },
+    { "coords": [-1745, 1100], "name": "Spawn", "icon": "green_circle", "size": 2 },
+    { "coords": [  298, -1030], "name": "Coche", "icon": "car", "size": 2 },
     { "coords": [-1780, 1760], "name": "Gasolinera", "icon": "gasStation", "size": 2 },
 ]

e9279f19714839ce2ddab0dc468d7b80f3db0662
Javier <javier.gonzalezberenguel@gmail.com>
2025-06-08 00:51:25 +0200

    # spawn, car, zoom, posoitions

--- data/js/humanitzCoords.js
+++ data/js/humanitzCoords.js
@@ -1,8 +1,9 @@
 const waypoints = [
     { "coords": [  298, -1030], "name": "Coche militar en torre de radio", "icon": "militaryTruck", "size": 2 },
     { "coords": [  360, -1040], "name": "Torre de radio. Necesita: batería coche, electronics, cable eléctrico, receptor radio", "icon": "radioTower", "size": 3 },
-    { "coords": [  760, -1260], "name": "Granja 2", "icon": "hut", "size": 3 },
-    { "coords": [ -495,  -310], "name": "Granja", "icon": "hut", "size": 3 },
-    { "coords": [-1745,  1100], "name": "Coche spawn", "icon": "pickupCar", "size": 2 },
+    { "coords": [  760, -1260], "name": "Granja 2", "icon": "hut", "size": 2 },
+    { "coords": [ -485,  -295], "name": "Granja", "icon": "hut", "size": 3 },
+    { "coords": [ -490,  -330], "name": "Coche", "icon": "militaryTruck", "size": 2 },
+    { "coords": [-1745,  1100], "name": "Spawn", "icon": "green_circle", "size": 2 },
     { "coords": [-1780,  1760], "name": "Gasolinera", "icon": "gasStation", "size": 3 },
 ]

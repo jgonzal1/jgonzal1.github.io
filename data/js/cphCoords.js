7c0c26378713d3729db8cc7f79b26502d9393827
Javier <javier.gonzalezberenguel@gmail.com>
2025-06-18 06:29:09 +0200

    + cphVisit

---
+++ data/js/cphCoords.js
@@ -0,0 +1,27 @@
+const waypoints = [
+    { "coords": [55.61,   12.42  ], "name": "Playa", "colors": ["#360","#6F7"]},
+    { "coords": [55.61,   12.64  ], "name": "Aeropuerto", "icon": "airport", "size": 3 },
+    { "coords": [55.61,   13.00  ], "name": "Malmö", "colors": ["#460","#CF7"]},
+    { "coords": [55.65,   12.08  ], "name": "Roskilde<br/>(museo vikingo)", "colors": ["#620","#F67"]},
+    { "coords": [55.65,   12.58  ], "name": "Amager Fælled", "colors": ["#640","#FC7"]},
+    { "coords": [55.6665, 12.4385], "name": "Casa (Brondbyøster)", "icon": "blockhouses", "size": 3 },
+    { "coords": [55.68,   12.59  ], "name": "Copenhagen<br/>(centro)", "colors": ["#640","#FC7"]},
+    { "coords": [55.70,   13.19  ], "name": "Lund<br/>(pueblo histórico)", "colors": ["#460","#CF7"]},
+    { "coords": [55.77,   12.50  ], "name": "Lyngby<br/>(mi antigua<br/>casa, bosque<br/>y lagos muy<br/>bonitos)", "colors": ["#630","#FA7"]},
+    { "coords": [55.80,   12.94  ], "name": "Järavallens<br/>Strövområde<br/>(parque natural)", "colors": ["#460","#CF7"]},
+    { "coords": [55.83,   12.06  ], "name": "Frederikssund<br/>(pueblo vikingo)", "colors": ["#620","#F67"]},
+    { "coords": [55.93,   12.30  ], "name": "Hillerød<br/>(castillo más<br/>importante de<br/>Dinamarca)", "colors": ["#660","#FF7"]},
+    { "coords": [55.97,   12.54  ], "name": "Louisiana<br/>(museo)", "colors": ["#630","#FA7"]},
+    { "coords": [56.03,   12.61  ], "name": "Elsinor<br/>(castillo de Hamlet)", "colors": ["#630","#FA7"]},
+    { "coords": [56.04,   12.03  ], "name": "Stængehus Strand<br/>(playa en parque natural)", "colors": ["#660","#FF7"]},
+];
+
+/*
+"#360","#6F7", F27
+"#460","#CF7", S28
+"#660","#FF7", D29
+"#640","#FC7", L30
+"#630","#FA7", M01
+"#620","#F67", X02
+"#610","#F37", J03
+*/

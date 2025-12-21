467e12b85043c111682c3b82098013853a15f1a3
Javier <javier.gonzalezberenguel@gmail.com>
2025-12-21 18:01:36 +0100

    + alasql, # increased end date

--- assets/tasksManager.js
+++ assets/tasksManager.js
@@ -11,7 +11,7 @@ console.log("totalHPerWeek", globalThis.totalHPerWeek);
 const nextViAsV = false;
 const msPerH = 3600000;
 const msPerD = msPerH * 24;
-const loadTasksUntilDate = "2026-01-07"; // inclusive
+const loadTasksUntilDate = "2026-01-08"; // inclusive
 // Number of days from today until loadTasksUntilDate
 globalThis.categoryAggrDaysRange = Math.ceil(
   (new Date(loadTasksUntilDate).getTime() - new Date().getTime()) / msPerD

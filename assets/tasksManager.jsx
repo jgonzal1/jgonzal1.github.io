b6ed933dfb2d339b662fd5e39d57ff1eea0ebbfa
Javier <javier.gonzalezberenguel@gmail.com>
2026-03-03 19:16:39 +0100

    # yellow for health checks

--- assets/tasksManager.jsx
+++ assets/tasksManager.jsx
@@ -691,7 +691,9 @@ class tasksManager extends globalThis.React.Component {
       // @ts-ignore
       goalsDom.innerHTML = `<table>
         <tr><th>Category</th>     <th>H/W</th>                                      <th>🎯YGoals</th></tr>
-        <tr><td>🍏/Health</td>    <td id="healthCount" class="bold-right">5</td>    <td>🩺checks, <span style="color:#e15759">🪁🏄ks</span></td></tr>
+        <tr><td>🍏/Health</td>    <td id="healthCount" class="bold-right">5</td>    <td>
+          <span style="color:#caa04c">🩺checks</span> <span style="color:#e15759">🪁🏄ks</span></td>
+        </tr>
         <tr><td>🏠💰/FIRE</td>    <td id="fireCount" class="bold-right">2.5</td>    <td id="fireGoal"><div class="bordered" style="color:#e15759">🏠♴💼</div></td></tr>
         <tr><td>🚩/Rels</td>      <td id="relsCount" class="bold-right">2</td>      <td style="color:#e15759">🚩🇸🇪🏠&💼</td></tr>
         <tr><td>🔬🌿/Motivs</td>  <td id="motivsCount" class="bold-right">1.5</td>  <td style="color:#b5bd68">h/XR or 400🌳</td></tr>

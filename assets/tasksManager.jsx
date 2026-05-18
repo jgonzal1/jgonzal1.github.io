67c36579ef9d51950a8f603e607194eec0c506d1
Javier González Berenguel <ext.javier.gonzales.berenguel@dsv.com>
2026-05-18 13:14:35 +0200

    # readj numbers

--- assets/tasksManager.jsx
+++ assets/tasksManager.jsx
@@ -712,11 +712,11 @@ class tasksManager extends globalThis.React.Component {
       let tc = 0;
       let tcl = 0;
       [
-        {"d":"healthCount", "l":3.5, "v":0, "s":["1.🍏"],               },
-        {"d":"fireCount",   "l":3.5, "v":0, "s":["2.🏠","3.💰"],        },
-        {"d":"relCount",    "l":2.5, "v":0, "s":["4.🚩"],               },
-        {"d":"motCount",    "l":2.0, "v":0, "s":["5.🌿","5.🔬"],        },
-        {"d":"restCount",   "l":0.5, "v":0, "s":["6.📺","7.🎮","8.🌐"], },
+        {"d":"healthCount", "l":3.3, "v":0, "s":["1.🍏"],               },
+        {"d":"fireCount",   "l":3.0, "v":0, "s":["2.🏠","3.💰"],        },
+        {"d":"relCount",    "l":2.7, "v":0, "s":["4.🚩"],               },
+        {"d":"motCount",    "l":2.6, "v":0, "s":["5.🌿","5.🔬"],        },
+        {"d":"restCount",   "l":2.5, "v":0, "s":["6.📺","7.🎮","8.🌐"], },
       ].map((k)=>{
         k["s"].map(l=>k["v"]+=parseFloat(
           // @ts-ignore
